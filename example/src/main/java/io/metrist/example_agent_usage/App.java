5cb6b3232c558132440d866c00b074b44c4d994c
Daven Casia <daven@metrist.io>
2022-09-26 10:09:57 -0600

    Added example

---
+++ example/src/main/java/io/metrist/example_agent_usage/App.java
@@ -0,0 +1,38 @@
+package com.mycompany.app;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class App {
+    private static final HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("https://httpbin.org/get"))
+                .setHeader("User-Agent", "Java 11 HttpClient Bot")
+                .build();
+
+        var response = httpClient.send(request, BodyHandlers.ofString());
+        // client.sendAsync(request, BodyHandlers.ofString())
+        // .thenApply(HttpResponse::body)
+        // .thenAccept(System.out::println)
+        // .join();
+        System.out.println(response.body());
+    }
+}
